a3f9d56da6acf2a83ecab6208c4c32cd1a28a140
Jasper Meiners <mail@jaspermeiners.com>
2022-04-27 18:15:47 +0200

    simplified, open source water

--- Assets/_Project/Scripts/ToggleTextDistortion.cs
+++ Assets/_Project/Scripts/ToggleTextDistortion.cs
@@ -6,8 +6,13 @@ public class ToggleTextDistortion : ToggleBase
     [SerializeField] private UserSettings _settings;
     private UserSettings Settings => _settings;
 
+    /*
     [SerializeField] private StylizedWaterURP _water;
     private StylizedWaterURP Water => _water;
+    */
+    
+    [SerializeField] private Material _water;
+    private Material Water => _water;
     
 
     private void Start()
@@ -24,8 +29,12 @@ public class ToggleTextDistortion : ToggleBase
 
     private void UpdateValue(bool value)
     {
+        /*
         Water.refractionStrength = value ? 0.06f : 0f;
         Water.WriteMaterialProperties();
+        */
+        
+        Water.SetFloat("_SettingsRefraction", value ? 1f : 0f);
         
         UpdateCheckSprite(value);
     }
