64c659e08f71d19eefb14d21135a346314a82c7a
Jasper Meiners <mail@jaspermeiners.com>
2022-04-27 23:08:51 +0200

    removed non-open source water shader

--- Assets/_Project/Scripts/ToggleTextDistortion.cs
+++ Assets/_Project/Scripts/ToggleTextDistortion.cs
@@ -1,4 +1,3 @@
-using StylizedWater;
 using UnityEngine;
 
 public class ToggleTextDistortion : ToggleBase
@@ -6,11 +5,6 @@ public class ToggleTextDistortion : ToggleBase
     [SerializeField] private UserSettings _settings;
     private UserSettings Settings => _settings;
 
-    /*
-    [SerializeField] private StylizedWaterURP _water;
-    private StylizedWaterURP Water => _water;
-    */
-    
     [SerializeField] private Material _water;
     private Material Water => _water;
     
@@ -29,11 +23,6 @@ public class ToggleTextDistortion : ToggleBase
 
     private void UpdateValue(bool value)
     {
-        /*
-        Water.refractionStrength = value ? 0.06f : 0f;
-        Water.WriteMaterialProperties();
-        */
-        
         Water.SetFloat("_SettingsRefraction", value ? 1f : 0f);
         
         UpdateCheckSprite(value);
